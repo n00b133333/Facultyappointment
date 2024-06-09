10c234f3230f825a94e69b4b721c4e8499c818fa
Henderson <gelidohenderson@gmail.com>
2024-06-09 08:06:44 +0800

    origin

--- admin/includes/admin_functions.php
+++ admin/includes/admin_functions.php
@@ -1,7 +1,7 @@
 <?php 
 include("../../db.php");
 
-function login($uname, $pass)
+function login($uname, $pass, $conn)
 {
 $sql = "SELECT * from admin where 'uname' = $uname AND 'pass' = $pass";
 $lsql = mysqli_query($conn,$sql);
